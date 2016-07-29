036fa2c3f044b9cbf670132a8732a421093bfa5d
urmas.tamm <urmas.tamm@cgi.com>
2016-07-29 14:21:07 +0300

    Local commit

---
+++ src/main/java/org/tamm/throttle/TestPage.java
@@ -0,0 +1,15 @@
+package org.tamm.throttle;
+
+public class TestPage {
+	public void print(int i)
+	{
+		if(i == 0)
+		{
+			System.out.println("Null");
+		}
+		else
+		{
+			System.out.println("Non-Null");
+		}
+	}
+}
