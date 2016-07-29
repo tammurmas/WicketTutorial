97b33c16abceee6ab92570f3cb187d148e7d2c5a
tammurmas <tammurmas@gmail.com>
2016-07-29 11:58:06 +0300

    Throttle file edited
    Throttle file edited

--- src/main/java/org/tamm/throttle/ThrottlePage.java
+++ src/main/java/org/tamm/throttle/ThrottlePage.java
@@ -9,7 +9,9 @@ import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.util.time.Duration;
-
+/**
+ * Throttle file edited
+ * */
 public class ThrottlePage extends WebPage {
 	private static final long serialVersionUID = 1L;
 	
