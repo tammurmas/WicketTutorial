9fdb8feb620fa501e3e80f4e7b0c805d910c286b
urmas.tamm <urmas.tamm@cgi.com>
2016-07-29 13:15:49 +0300

    local change

--- src/main/java/org/tamm/throttle/ThrottlePage.java
+++ src/main/java/org/tamm/throttle/ThrottlePage.java
@@ -10,7 +10,7 @@ import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.util.time.Duration;
 /**
- * Throttle file edited remotely
+ * Throttle file edited locally
  */
 public class ThrottlePage extends WebPage {
 	private static final long serialVersionUID = 1L;
